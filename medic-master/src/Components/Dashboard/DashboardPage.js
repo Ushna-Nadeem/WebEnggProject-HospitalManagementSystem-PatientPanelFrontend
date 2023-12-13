45daed001346d1d357c4e0b37a49e8c16421a64c
Ushna-Nadeem <ushna.2002.ushna@gmail.com>
2023-12-13 22:36:59 +0500

    DashboardPage

--- medic-master/src/Components/Dashboard/DashboardPage.js
+++ medic-master/src/Components/Dashboard/DashboardPage.js
@@ -1,12 +1,67 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './DashboardPage.css';
 
-const Dashboard = () => {
-  // Component logic here
+const DashboardPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
   return (
-    <div>
-      {/* Dashboard content */}
+    <div className="dashboard-container">
+      <div className="header-bar-dashboard">
+        <button className="logout-button" onClick={handleLogout}>
+          <b>Logout</b>
+        </button>
+      </div>
+      <h2>Dashboard</h2>
+      <div className="dashboard-options">
+        <Link to="/dashboard/profile">
+          <img
+            className="option-icon"
+            src="\images\profile.png"
+            alt="Profile"
+          />
+          My Profile
+        </Link>
+        <Link to="/dashboard/appointments">
+          <img
+            className="option-icon"
+            src="\images\appointment.jpeg"
+            alt="Appointments"
+          />
+          Appointments
+        </Link>
+        <Link to="/dashboard/prescriptions">
+          <img
+            className="option-icon"
+            src="\images\prescription.jpeg"
+            alt="Prescriptions"
+          />
+          Prescriptions
+        </Link>
+        <Link to="/dashboard/bills">
+          <img
+            className="option-icon"
+            src="\images\bills.jpeg"
+            alt="Bills"
+          />
+          Bills
+        </Link>
+        <Link to="/dashboard/payment">
+          <img
+            className="option-icon"
+            src="\images\payment.jpeg"
+            alt="Payment"
+          />
+          Payment
+        </Link>
+      </div>
     </div>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
--- medic-master/src/index.js
+++ medic-master/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/Home/HomePage';
 import RegisterPage from './Components/Register/RegisterPage';
 import LoginPage from './Components/Login/LoginPage';
-import Dashboard from './Components/Dashboard/DashboardPage';
+import DashboardPage from './Components/Dashboard/DashboardPage';
 import './index.css';
 
 ReactDOM.render(
@@ -13,7 +13,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
